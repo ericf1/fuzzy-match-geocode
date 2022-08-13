5436f4612c707c42a9865c0e6a02058fdba0c8b2
Eric <89803837+ericf1@users.noreply.github.com>
2022-08-13 03:47:02 -0400

    Update geocode_by_fuzzy_match.py

--- geocode_by_fuzzy_match.py
+++ geocode_by_fuzzy_match.py
@@ -5,110 +5,168 @@ import concurrent.futures
 # must also pip install pyarrow
 
 need_to_geocode_df = pd.read_parquet(
-    'standardized_addr_eric.parquet', engine='pyarrow')
+    'standardized_addr_eric.parquet', engine='pyarrow')[["stnd_type", "stnd_logr",
+                                                        "stnd_numero", "logradouro", "numero", "codigo_do_municipio_no_bcbase"]]
 key_addresses_df = pd.read_parquet(
-    'Brazil-Latest-points.parquet', engine='pyarrow')
+    'Brazil-Latest-points.parquet', engine='pyarrow')[["id", "housenumber", "street", "code_muni"]]
 key_streets_df = pd.read_parquet(
-    'Brazil-Latest-lines.parquet', engine='pyarrow')
+    'Brazil-Latest-lines.parquet', engine='pyarrow')[["id", "street", "code_muni"]]
+
+# merging the muni tables
+muni_code_df = pd.read_parquet('muni_id.parquet', engine="pyarrow")[
+    ["code_muni", "codigo_do_municipio_no_bcbase"]]
+need_to_geocode_df = need_to_geocode_df.astype(
+    {"codigo_do_municipio_no_bcbase": int}, errors="raise")
+muni_code_df = muni_code_df.astype(
+    {"codigo_do_municipio_no_bcbase": int}, errors="raise")
+need_to_geocode_df = pd.merge(need_to_geocode_df, muni_code_df,
+                              on=["codigo_do_municipio_no_bcbase"])
 
 
 def perfect_match_by_muni_housenumber(addr):
     # matches directly the municipality and the housenumber using the points parquet
     addr = addr.iloc[0]
-    if addr["stnd_numero"]:
-        housenumber = addr["stnd_numero"]
-    else:
-        housenumber = addr["numero"]
-    match = key_addresses_df[(key_addresses_df["housenumber"] == housenumber) & (
-        key_addresses_df["name_muni"] == addr["muni"]) & (key_addresses_df["name_state"] == addr["state"])]
-    return match
+    # we first want to use the stnd number if avaliable
+    stnd_numero = addr["stnd_numero"]
+    if stnd_numero:
+        return key_addresses_df[(key_addresses_df["housenumber"] == stnd_numero) & (
+            key_addresses_df["code_muni"] == addr["code_muni"])]
+    return key_addresses_df[(key_addresses_df["housenumber"] == addr["numero"]) & (
+        key_addresses_df["code_muni"] == addr["code_muni"])]
 
 
 def perfect_match_by_muni(addr):
     # matches directly only by the municipality using the lines parquet
-    return key_streets_df[(key_streets_df["name_muni"] == addr.iloc[0]["muni"])]
+    return key_streets_df[(key_streets_df["code_muni"] == addr.iloc[0]["code_muni"])]
 
 
 def stnd_parse_str(street_name):
     # parsing the street name string for redundant info
-    return str(street_name).replace("Rua ", "")
+    return str(street_name).replace("Rua ", "").replace("Avenida ", "")
 
 
-def find_max_jaro_winkler_by_street(df, addr, similarity_ratio):
+def find_best_match(df, addr, distance_metric):
     # given a df and an address, finds the closest matching street name with at least the given distance value
-    largest_similar = -1
-    most_similar_row = None
     addr = addr.iloc[0]
-    for index, row in df.iterrows():
-        if addr["stnd_logr"]:
-            # using the jaro winkler formula to see level of similarity: https://en.wikipedia.org/wiki/Jaro%E2%80%93Winkler_distance
-            cur_jaro_winkler_distance = jaro_winkler(
-                stnd_parse_str(row["street"]), stnd_parse_str(addr["stnd_logr"]))
-        else:
-            cur_jaro_winkler_distance = jaro_winkler(
-                stnd_parse_str(row["street"]), stnd_parse_str(addr["logradouro"]))
-        if cur_jaro_winkler_distance > largest_similar:
-            largest_similar = cur_jaro_winkler_distance
-            most_similar_row = row
-    if largest_similar > similarity_ratio:
-        return most_similar_row
+    if addr["stnd_logr"]:
+        street_name = addr["stnd_logr"]
+    else:
+        street_name = addr["logradouro"]
+
+    for idx in range(0, df.shape[0], 1):
+        df_street_name = stnd_parse_str(df["street"].iloc[idx])
+        if df_street_name[0] != street_name[0]:
+            continue
+        if not (len(df_street_name) - 5 <= len(street_name) <= len(df_street_name) + 5):
+            continue
+        cur_jaro_winkler_distance = jaro_winkler(
+            (df_street_name), (street_name))
+        if cur_jaro_winkler_distance >= distance_metric:
+            code_muni = df["code_muni"].iloc[idx]
+            id = df["id"].iloc[idx]
+            return pd.DataFrame(data={"code_muni": code_muni, "id": id, "process": [3], "street": df["street"].iloc[idx], "jaro_distance": cur_jaro_winkler_distance})
+            # return pd.DataFrame(data={"code_muni": code_muni, "id": id, "process": [3]})
     return None
 
 
-def fuzzy_match_by_street_name(addr):
+def perfect_match_by_street_name(addr):
+    addr = addr.iloc[0]
+    if not addr['stnd_type']:
+        return
+    street_name = f"{addr['stnd_type']} {addr['stnd_logr']}"
+    match = key_streets_df[(key_streets_df["street"] == street_name) & (
+        key_streets_df["code_muni"] == addr["code_muni"])]
+    if match.empty:
+        return None
+    return match
+
+
+def perfect_match_by_street_name_points_data(addr, df):
+    addr = addr.iloc[0]
+    if not addr['stnd_type']:
+        return
+    street_name = f"{addr['stnd_type']} {addr['stnd_logr']}"
+    match = df[(df["street"] == street_name)]
+    if match.empty:
+        return
+    match = match.iloc[:1]
+    match["process"] = [1]
+    return match
+
+
+def points_data_match(addr):
     # fuzzy matching the address
-    df = perfect_match_by_muni_housenumber(addr).reset_index()
+    df = perfect_match_by_muni_housenumber(addr)
     if df.empty:
         return None
-    return find_max_jaro_winkler_by_street(df, addr, .95)
+    perfect_match = perfect_match_by_street_name_points_data(addr, df)
+    if perfect_match is not None:
+        return perfect_match
+    return find_best_match(df, addr, .95)
 
 
-def fuzzy_match_by_street_only(addr):
+def streets_data_match(addr):
     # fuzzy matching only the street and using the lines parquet
-    df = perfect_match_by_muni(addr).reset_index()
-    return find_max_jaro_winkler_by_street(df, addr, .95)
+    df = perfect_match_by_muni(addr)
+    # if we can't match the muni then, we should just return
+    if df.empty:
+        return None
+    output = perfect_match_by_street_name(addr)
+    if not output is None:
+        # restricts to only one street match
+        output = output.iloc[:1]
+        output["process"] = [2]
+        return output
+    return find_best_match(df, addr, .95)
 
 
 def perform_matches(addr):
-    # Process 1
-    street_name_match = fuzzy_match_by_street_name(addr)
-    if not street_name_match is None:
-        return [addr, street_name_match, "by street"]
-    # Process 2
-    street_only_match = fuzzy_match_by_street_only(addr)
+    # points data
+    points_street_match = points_data_match(addr)
+    if not points_street_match is None:
+        return [addr, points_street_match, "points_data"]
+    # streets data
+    street_only_match = streets_data_match(addr)
     if not street_only_match is None:
-        return [addr, street_only_match, "by street and housenumber"]
+        return [addr, street_only_match, "street_data"]
     # Both failed (Not similar enough to anything)
     return [addr, None]
 
 
+def export_as_csv(df, name):
+    return df.to_csv(name, index=False, header=True, encoding="utf-8")
+
+
 def main():
     start = time.perf_counter()
-    df = pd.DataFrame()
+    df_output = pd.DataFrame()
     df_failed_addresses = pd.DataFrame()
     # number of threads allocated to this python script
     number_of_cores = 4
     with concurrent.futures.ProcessPoolExecutor(max_workers=number_of_cores) as executor:
         results = executor.map(perform_matches, [
-            need_to_geocode_df.loc[[i]] for i in range(len(need_to_geocode_df.index))])
+            need_to_geocode_df.loc[[i]] for i in range(0, 100)])
         for i, result in enumerate(results):
             df_sent = result[0]
-            series_most_similar = result[1]
-            if series_most_similar is None:
+            df_most_similar = result[1]
+            if df_most_similar is None or df_most_similar.empty:
                 # there were not matching streets that were close enough
+                df_sent["stnd_addr_index"] = df_sent.index[0]
                 df_failed_addresses = pd.concat([df_failed_addresses, df_sent])
                 continue
-            process = result[2]
-            df_most_similar = series_most_similar.to_frame().T
-            df = pd.concat([df, df_most_similar.merge(df_sent, how="cross")])
+            df_most_similar["standardized_addr_index"] = df_sent.index[0]
+            df_most_similar["dataset"] = result[2]
+            original_street_name = df_sent.iloc[0]["stnd_logr"]
+            if not original_street_name:
+                original_street_name = df_sent.iloc[0]["logradouro"]
+            df_most_similar["original_street_name"] = original_street_name
+            df_output = pd.concat([df_output, df_most_similar])
     finish = time.perf_counter()
-    # df.to_parquet('addresses_matched.parquet.gzip', compression='gzip')
-    # df_failed_addresses.to_parquet(
-    #     'df_failed_addresses.parquet.gzip', compression='gzip')
-    df.to_csv('addresses_matched.csv', index=False,
-              header=True, encoding="utf-8")
-    df_failed_addresses.to_csv(
-        'failed_addresses.csv', index=False, header=True, encoding="utf-8")
+    df_output = df_output.reset_index()
+    print(df_output)
+
+    export_as_csv(df_output, 'addresses_matched.csv')
+    export_as_csv(df_failed_addresses, 'failed_addresses.csv')
     print(f"This took {round(finish - start, 5)} seconds")
 
 
