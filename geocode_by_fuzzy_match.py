f5a0dff30119db72f7c2937c2c7c7c9a45f91d76
Eric <89803837+ericf1@users.noreply.github.com>
2022-08-19 01:41:43 -0500

    Update geocode_by_fuzzy_match.py

--- geocode_by_fuzzy_match.py
+++ geocode_by_fuzzy_match.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import time
 from Levenshtein import jaro_winkler
 import concurrent.futures
@@ -6,21 +7,25 @@ import concurrent.futures
 
 need_to_geocode_df = pd.read_parquet(
     'standardized_addr_eric.parquet', engine='pyarrow')[["stnd_type", "stnd_logr",
-                                                        "stnd_numero", "logradouro", "numero", "codigo_do_municipio_no_bcbase"]]
+                                                        "stnd_numero", "logradouro", "numero", "codigo_do_municipio_no_bcbase", "row_id"]]
 key_addresses_df = pd.read_parquet(
     'Brazil-Latest-points.parquet', engine='pyarrow')[["id", "housenumber", "street", "code_muni"]]
 key_streets_df = pd.read_parquet(
     'Brazil-Latest-lines.parquet', engine='pyarrow')[["id", "street", "code_muni"]]
-
 # merging the muni tables
 muni_code_df = pd.read_parquet('muni_id.parquet', engine="pyarrow")[
     ["code_muni", "codigo_do_municipio_no_bcbase"]]
+# ensuring type errors are not wrong
 need_to_geocode_df = need_to_geocode_df.astype(
     {"codigo_do_municipio_no_bcbase": int}, errors="raise")
 muni_code_df = muni_code_df.astype(
     {"codigo_do_municipio_no_bcbase": int}, errors="raise")
 need_to_geocode_df = pd.merge(need_to_geocode_df, muni_code_df,
                               on=["codigo_do_municipio_no_bcbase"])
+
+# static values:
+NUMBER_OF_CORES = 1
+NUMBER_OF_CHUNKS = 10
 
 
 def perfect_match_by_muni_housenumber(addr):
@@ -56,9 +61,9 @@ def find_best_match(df, addr, distance_metric):
     for idx in range(0, df.shape[0], 1):
         df_street_name = stnd_parse_str(df["street"].iloc[idx])
         # if df_street_name[0] != street_name[0]:
-            # continue
+        # continue
         # if not (len(df_street_name) - 5 <= len(street_name) <= len(df_street_name) + 5):
-            # continue
+        # continue
         cur_jaro_winkler_distance = jaro_winkler(
             (df_street_name), (street_name))
         if cur_jaro_winkler_distance >= distance_metric:
@@ -137,24 +142,23 @@ def export_as_csv(df, name):
     return df.to_csv(name, index=False, header=True, encoding="utf-8")
 
 
-def main():
+def geocode_section(section_of_df, chunk_num):
     start = time.perf_counter()
     df_output = pd.DataFrame()
     df_failed_addresses = pd.DataFrame()
     # number of threads allocated to this python script
-    number_of_cores = 4
-    with concurrent.futures.ProcessPoolExecutor(max_workers=number_of_cores) as executor:
+    with concurrent.futures.ProcessPoolExecutor(max_workers=NUMBER_OF_CORES) as executor:
         results = executor.map(perform_matches, [
-            need_to_geocode_df.loc[[i]] for i in range(0, 100)])
-        for i, result in enumerate(results):
+            section_of_df.iloc[[i]] for i in range(0, 1)])
+        for result in results:
             df_sent = result[0]
             df_most_similar = result[1]
             if df_most_similar is None or df_most_similar.empty:
                 # there were not matching streets that were close enough
-                df_sent["stnd_addr_index"] = df_sent.index[0]
+                df_sent["stnd_addr_index"] = df_sent.iloc[0]["row_id"]
                 df_failed_addresses = pd.concat([df_failed_addresses, df_sent])
                 continue
-            df_most_similar["standardized_addr_index"] = df_sent.index[0]
+            df_most_similar["standardized_addr_index"] = df_sent.iloc[0]["row_id"]
             df_most_similar["dataset"] = result[2]
             original_street_name = df_sent.iloc[0]["stnd_logr"]
             if not original_street_name:
@@ -162,12 +166,19 @@ def main():
             df_most_similar["original_street_name"] = original_street_name
             df_output = pd.concat([df_output, df_most_similar])
     finish = time.perf_counter()
-    df_output = df_output.reset_index()
+    df_output = df_output.reset_index(drop=True)
     print(df_output)
 
-    export_as_csv(df_output, 'addresses_matched.csv')
-    export_as_csv(df_failed_addresses, 'failed_addresses.csv')
+    export_as_csv(df_output, f'addresses_matched{chunk_num}.csv')
+    export_as_csv(df_failed_addresses, f'failed_addresses{chunk_num}.csv')
     print(f"This took {round(finish - start, 5)} seconds")
+
+
+def main():
+    need_to_geocode_df_parts = np.array_split(
+        need_to_geocode_df, NUMBER_OF_CHUNKS)
+    for idx, df in enumerate(need_to_geocode_df_parts):
+        geocode_section(df, idx)
 
 
 if __name__ == "__main__":
